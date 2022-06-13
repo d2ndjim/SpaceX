5ec6fc1189e6b1a3ab1ce85d4a07d03c86235365
Jake Felice <jasonfelice0@gmail.com>
2022-06-13 07:10:52 -0700

    Create a fetch method and add reducers

--- src/redux/dragons/dragonsSlice.js
+++ src/redux/dragons/dragonsSlice.js
@@ -1,9 +1,31 @@
 import { createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const URL = 'https://api.spacexdata.com/v3/dragons';
+
+export const getDragons = () => async () => {
+  const res = await axios.get(URL);
+  const dragons = res.data;
+  const dragonInfo = dragons.map((dragon) => ({
+    name: dragon.name,
+    id: dragon.id,
+    description: dragon.description,
+    reserved: false,
+  }));
+  return dragonInfo;
+};
 
 const options = {
   name: 'dragons',
   initialState: [],
-  reducers: {},
+  reducers: {
+    reserveDragonsToggle: (state, action) => state.map((dragon) => {
+      if (dragon.id === action.payload) {
+        return { ...dragon, reserved: !dragon.reserved };
+      }
+      return dragon;
+    }),
+  },
 };
 
 const dragonsSlice = createSlice(options);

01d89337bd2b3c5543c775c6068935c8fbb43e7f
Jake Felice <jasonfelice0@gmail.com>
2022-06-13 09:29:27 -0700

    Use createAsyncThunk instead of axios

--- src/pages/Dragons/Dragons.js
+++ src/pages/Dragons/Dragons.js
@@ -1,20 +1,27 @@
 /* eslint-disable no-unused-vars */
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { getDragons, selectDragons, reserveDragonsToggle as toggleReserve } from '../../redux/dragons/dragonsSlice';
+import { getDragons, updateReserved } from '../../redux/dragons/dragonsSlice';
 
 const Dragons = () => {
-  const dragons = useSelector(selectDragons);
+  const dragons = useSelector((state) => state);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getDragons());
   }, [dispatch]);
 
+  const clickHandler = (id) => {
+    dispatch(updateReserved(id));
+  };
+
   return (
     <>
       <div>
-        <h1>Hello!</h1>
+        Hello
+        {dragons.map((dragon) => (
+          <h1 key={dragon.id}>{dragon.name}</h1>
+        ))}
       </div>
     </>
   );
--- src/redux/dragons/dragonsSlice.js
+++ src/redux/dragons/dragonsSlice.js
@@ -1,34 +1,37 @@
-import { createSlice } from '@reduxjs/toolkit';
-import axios from 'axios';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const URL = 'https://api.spacexdata.com/v3/dragons';
 
-export const getDragons = () => async () => {
-  const res = await axios.get(URL);
-  const dragons = res.data;
-  const dragonInfo = dragons.map((dragon) => ({
-    name: dragon.name,
-    id: dragon.id,
-    description: dragon.description,
-    reserved: false,
-  }));
-  return dragonInfo;
-};
+export const getDragons = createAsyncThunk(
+  'dragons/getDragons', async () => {
+    const res = await fetch(URL);
+    const dragons = await res.json();
+    const dragonInfo = dragons.map((dragon) => ({
+      name: dragon.name,
+      id: dragon.id,
+      description: dragon.description,
+      reserved: false,
+    }));
+    return dragonInfo;
+  },
+);
 
 const options = {
   name: 'dragons',
   initialState: [],
   reducers: {
-    reserveDragonsToggle: (state, action) => state.map((dragon) => {
+    updateReserved: (state, action) => state.map((dragon) => {
       if (dragon.id === action.payload) {
         return { ...dragon, reserved: !dragon.reserved };
       }
       return dragon;
     }),
   },
+  extraReducer: {
+    [getDragons.fulfilled]: (state, action) => action.payload,
+  },
 };
 
 const dragonsSlice = createSlice(options);
-export const selectDragons = (state) => state.dragons;
-export const { reserveDragonsToggle } = dragonsSlice.actions;
+export const { updateReserved } = dragonsSlice.actions;
 export default dragonsSlice.reducers;

a2c75eb4bec31e1b686bdae2d8c84ccb8d13f9bb
Jake Felice <jasonfelice0@gmail.com>
2022-06-13 06:17:09 -0700

    Add dragonsSlice in a seperate folder

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rocketReducer from './rocketsSlice';
 import missionReducer from './missionsSlice';
-import dragonReducer from './dragonsSlice';
+import dragonReducer from './dragons/dragonsSlice';
 
 const store = configureStore({
   reducer: {

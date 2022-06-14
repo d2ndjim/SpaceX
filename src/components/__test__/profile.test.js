ab8cdd6e606b9127dfa15244f1094c66c467d87c
Lekan <jimoridwanlekan@gmail.com>
2022-06-14 10:37:54 +0100

    create and test screen snapshots with the group

---
+++ src/components/__test__/profile.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import { Provider } from "react-redux";
+import store from "../../redux/configureStore";
+import Profile from "../profile";
+
+describe("Profiles test", () => {
+  it("renders profiles", () => {
+    const app = renderer
+      .create(
+        <Provider store={store}>
+          <Profile />
+        </Provider>
+      )
+      .toJSON();
+    expect(app).toMatchSnapshot();
+  });
+});
